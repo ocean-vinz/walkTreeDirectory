6ec4b9761919caf3e0200d37947bfbee18a09575
ocean-vinz <hello@irvino.com>
2022-03-13 15:39:18 +1300

    adding summary after the execution finished

--- WalkTreeDir.py
+++ WalkTreeDir.py
@@ -10,10 +10,18 @@ def checkfiles():
     if minNumberFiles.isnumeric():
         print('Checking the amount of files in ' + str(currentdir))
         print()
+        currentcount=0
         for foldername, subfolder, filename in os.walk(currentdir):
             numberoffiles=len(filename)
             if numberoffiles >= int(minNumberFiles):
                 print(foldername + ' there are ' + str(numberoffiles) + ' files')
+                if numberoffiles > currentcount:
+                    currentcount=numberoffiles
+                    highestfolder=foldername
+        print()
+        print('SUMMARY: ')
+        print('Checking starts from this parent folder: ' + str(currentdir))
+        print(f'The highest count is inside folder {highestfolder} with the total of {currentcount} files')
     else:
         print("Please only input an integer/numeric!")
 
